6d33adc296b981922be68312064794eb7eab9d75
alexandermargraf <alexander.margraf@bluewin.ch>
2022-07-15 13:37:20 +0200

    init

---
+++ src/main/java/ch/am/axa/backend/BackendApplication.java
@@ -0,0 +1,14 @@
+package ch.am.axa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		System.out.println("a");
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
