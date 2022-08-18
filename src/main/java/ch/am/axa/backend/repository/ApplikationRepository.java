b9c60955117116787bb1983357fd33415217c572
alexandermargraf <alexander.margraf@bluewin.ch>
2022-08-18 13:48:40 +0200

    ApplikationRepository

--- src/main/java/ch/am/axa/backend/repository/ApplikationRepository.java
+++ src/main/java/ch/am/axa/backend/repository/ApplikationRepository.java
@@ -3,5 +3,9 @@ package ch.am.axa.backend.repository;
 import ch.am.axa.backend.entity.ApplikationEntity;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * @author : Alexander Margraf
+ * @version : 18.08.2022
+ */
 public interface ApplikationRepository extends CrudRepository<ApplikationEntity, String> {
 }
