62cf04ebd338394efddb8cf031cd351464373de7
alexandermargraf <alexander.margraf@bluewin.ch>
2022-08-18 14:00:59 +0200

    DataService

--- src/main/java/ch/am/axa/backend/service/DataService.java
+++ src/main/java/ch/am/axa/backend/service/DataService.java
@@ -1,94 +1,87 @@
 package ch.am.axa.backend.service;
 
-
 import ch.am.axa.backend.entity.ApplikationEntity;
-import ch.am.axa.backend.entity.Language;
-import ch.am.axa.backend.entity.RestDataStructur;
-import ch.am.axa.backend.entity.TranslationEntry;
+import ch.am.axa.backend.entity.TranslationEntity;
 import ch.am.axa.backend.repository.ApplikationRepository;
-import ch.am.axa.backend.repository.LanguageRepository;
-import ch.am.axa.backend.repository.TranslationEntryRepository;
+import ch.am.axa.backend.repository.TranslationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * @author : Alexander Margraf
+ * @version : 18.08.2022
+ */
 @Service
 @CrossOrigin
 public class DataService {
 
     private final ApplikationRepository applikationRepository;
 
-    private final LanguageRepository languageRepository;
-
-    private final TranslationEntryRepository translationEntryRepository;
+    private final TranslationRepository translationEntryRepository;
 
     @Autowired
-    public DataService(ApplikationRepository applikationRepository,LanguageRepository languageRepository, TranslationEntryRepository translationEntryRepository) {
+    public DataService(ApplikationRepository applikationRepository, TranslationRepository translationEntryRepository) {
         this.applikationRepository = applikationRepository;
-        this.languageRepository = languageRepository;
         this.translationEntryRepository = translationEntryRepository;
     }
 
-    public Iterable<Language> getAllLanguages(){return languageRepository.findAll();}
-
-    public Iterable<TranslationEntry> getAllTranslationEntrys(){
-        List<RestDataStructur> data = new ArrayList<>();
-
-        Iterable<TranslationEntry> translationEntries = translationEntryRepository.findAll();
-
-        for(TranslationEntry e: translationEntries){
-            String id = e.getText_id();
-
-        }
+    public Iterable<TranslationEntity> getAllTranslationEntrys(){
+        Iterable<TranslationEntity> translationEntries = translationEntryRepository.findAll();
 
         return translationEntries;
     }
 
-    public List<RestDataStructur> getAllTranslationGermany(){
-
-        List<RestDataStructur> structurList = new ArrayList<>();
-
-        List<TranslationEntry> entries = new ArrayList<>();
-        Iterable<TranslationEntry> translationEntries = translationEntryRepository.findAll();
-
-        for(TranslationEntry e: translationEntries){
-            if(e.getLang_idfs().getLang_id().equals("4")){
-                entries.add(e);
-            }
-        }
-
-        for(TranslationEntry e: entries){
-            List<Language> languageList = new ArrayList<>();
-            RestDataStructur structur = new RestDataStructur();
-
-            structur.setEntry(e);
-
-            List<TranslationEntry> entriesFromTextId = translationEntryRepository.findAllByText(e.getText_id());
-
-            for(TranslationEntry entry: entriesFromTextId){
-                languageList.add(entry.getLang_idfs());
-            }
-
-            structur.setLanguageList(languageList);
-
-            structurList.add(structur);
-        }
-
-        return structurList;
-    }
-
-    public Optional<Language> getLanguageByTextId(String id){
-        return languageRepository.findById(id);
-    }
-
-
-    public Optional<TranslationEntry> getTranslationEntryById(String id){
+    public Optional<TranslationEntity> getTranslationEntryById(String id){
         return translationEntryRepository.findById(id);
     }
+
+    public Iterable<TranslationEntity> getTranslationByAppId(String appId){
+        List<TranslationEntity> entities = (List<TranslationEntity>) getAllTranslationEntrys();
+
+        for(TranslationEntity e : entities){
+            if(!e.getApp_idfs().equals(appId)){
+                entities.remove(e);
+            }
+        }
+
+        return entities;
+    }
+
+    public void deleteTranslation(String textid){
+        translationEntryRepository.deleteById(textid);
+    }
+
+    public TranslationEntity addTranslation(TranslationEntity entity){
+
+        TranslationEntity newEntity = new TranslationEntity();
+
+        Optional<ApplikationEntity> applikationDb = applikationRepository.findById(entity.getApp_idfs().getApp_id());
+
+        if(applikationDb.isPresent()){
+            newEntity.setApp_idfs(applikationDb.get());
+        }else{
+            ApplikationEntity newApplikation = new ApplikationEntity();
+            newApplikation.setApp_id(entity.getApp_idfs().getApp_id());
+            newApplikation.setApp_owner_email(entity.getApp_idfs().getApp_owner_email());
+
+            newEntity.setApp_idfs(newApplikation);
+        }
+
+        newEntity.setText_id(entity.getText_id());
+        newEntity.setApp_idfs(entity.getApp_idfs());
+        newEntity.setContent_de(entity.getContent_de());
+        newEntity.setContent_fr(entity.getContent_fr());
+        newEntity.setContent_en(entity.getContent_en());
+        newEntity.setContent_it(entity.getContent_it());
+
+        return translationEntryRepository.save(entity);
+    }
+
+
 
 
 
